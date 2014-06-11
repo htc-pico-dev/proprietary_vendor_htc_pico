a0dc9312f241f5308542c47ee70d6ea762cb86b8
Arslan Kirmani <arslankirmani9600@gmail.com>
2014-06-11 04:10:22 -0700

    moved kk 2.7 blobs to system/vendor

--- vendor_pico.mk
+++ vendor_pico.mk
@@ -58,19 +58,20 @@ PRODUCT_COPY_FILES += \
     vendor/htc/pico/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
     vendor/htc/pico/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
     vendor/htc/pico/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-    vendor/htc/pico/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/pico/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
-    vendor/htc/pico/lib/libOpenCL.so:system/lib/libOpenCL.so \
-    vendor/htc/pico/lib/libOpenVG.so:system/lib/libOpenVG.so \
-    vendor/htc/pico/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-    vendor/htc/pico/lib/libC2D2.so:system/lib/libC2D2.so \
-    vendor/htc/pico/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
-    vendor/htc/pico/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    vendor/htc/pico/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    vendor/htc/pico/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/htc/pico/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/pico/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
-    vendor/htc/pico/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so 
+    vendor/htc/pico/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/htc/pico/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/htc/pico/lib/libllvm-arm.so:system/vendor/lib/libllvm-arm.so \
+    vendor/htc/pico/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/htc/pico/lib/libOpenVG.so:system//vendorlib/libOpenVG.so \
+    vendor/htc/pico/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
+    vendor/htc/pico/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/htc/pico/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
+    vendor/htc/pico/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
+    vendor/htc/pico/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
+    vendor/htc/pico/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/htc/pico/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/htc/pico/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/egl/libGLESv2S3D_adreno.so \
+    vendor/htc/pico/lib/egl/libq3dtools_adreno.so:system/vendorlib/egl/libq3dtools_adreno.soe 
 
 # RIL
 PRODUCT_COPY_FILES += \
